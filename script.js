f416765114dab06e0ec90ab5a0520c0f8205231d
Sofia <sofia.lorena@hotmail.com>
2024-01-24 00:45:05 -0500

    aumento de archivos

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+var viewer1 = OpenSeadragon({
+    showNavigator: true,
+    id: 'openseadragon1',
+    prefixUrl: 'openseadragon/openseadragon-bin-4.1.0/images/',
+    tileSources: '/Archivo/myoutputfolder.dzi'
+});
