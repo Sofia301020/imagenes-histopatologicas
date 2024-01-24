e8933725b3a86c5c24dfafa35608fa273e33c23c
Sofia <sofia.lorena@hotmail.com>
2024-01-24 00:18:11 -0500

    cambios

--- appr.js
+++ appr.js
@@ -6,7 +6,7 @@ const app = express()
 app.get('/',(req,res) => {
 console.log('Peticion recibida')
 
-res.send('<h1>Hola mundo soy yo </h1>')
+res.send('<h1>Hola mundo </h1>')
 })
 
 const puerto = process.env.puerto || 4000
