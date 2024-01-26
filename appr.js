888ee1ca0ec1a147b6f6582f5c3a09172c8bd076
Sofia <sofia.lorena@hotmail.com>
2024-01-25 23:03:23 -0500

    cambios 2

--- appr.js
+++ appr.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('dotenv').config()
 
 const app = express()
+app.use(express.static('public'));
 
 app.get('/',(req,res) => {
 console.log('Peticion recibida')
